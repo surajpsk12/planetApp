0c9c1d0146b3108225096f4792c300c1936bf048
Suraj Kumar <sk658139@gmail.com>
2025-04-17 21:21:49 +0530

    added comment in activity.xml

--- app/src/main/java/com/surajvanshsv/planetsapp/MainActivity.java
+++ app/src/main/java/com/surajvanshsv/planetsapp/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyCustomAdapter(getApplicationContext(), planetArrayList);
 
         listView.setAdapter(adapter);
-
+        // on click listener for this to show toast message on this
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
